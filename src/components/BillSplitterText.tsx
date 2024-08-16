8b09eb3950e9a0569aaa0d072b3427c6333cddbd
Connor Henry <cjhenry10@gmail.com>
2024-08-16 10:15:52 -0400

    adjusted some margin on bill splitter text

--- src/components/BillSplitterText.tsx
+++ src/components/BillSplitterText.tsx
@@ -1,13 +1,15 @@
 function BillSplitterText() {
   return (
     <div className='mb-4 leading-7'>
-      <p>Share equal savings with everyone who shares your monthly expenses.</p>
-      <p>
+      <p className='mb-4'>
+        Share equal savings with everyone who shares your monthly expenses.
+      </p>
+      <p className='my-4'>
         Enter your monthly expenses and your income, along with the income of
         each of the people with whom you share expenses — be they a roommate,
         family member, friend, or partner.
       </p>
-      <p>
+      <p className='mt-4'>
         This calculator is designed to give each person helping out with bills,
         groceries, and other costs an equal amount of money to save after they
         have contributed to the expenses.
